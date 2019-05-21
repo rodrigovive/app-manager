98b3074ce6d3e12ec08e8ce546c06b1b2e6f0f12
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-05-21 14:37:23 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ const taskRouter = require("./routers/task");
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+app.get('/',(req,res) => {
+  res.send({
+    info: 'APP MANAGER'
+  })
+})
 
 app.listen(PORT, () => {
   console.log("Server is up on port ", PORT);
